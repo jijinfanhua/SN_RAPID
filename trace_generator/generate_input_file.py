e8e9fe145978e0ecbae75a0670f28c55b4a3aaff
jijinfanhua <1446353452@qq.com>
2023-07-10 11:18:08 +0800

    update generate_input_file

--- trace_generator/generate_input_file.py
+++ trace_generator/generate_input_file.py
@@ -6,6 +6,11 @@
 #   3. number of flows
 #   4-4+number of flows:  flow id, arrival rate of each flow
 
+# 1. 确定要生成的流数量、数据包个数、吞吐（确定时钟数）；
+# 2. 按照流数量，使用zipf程序生成每流包数量；
+# 3. 生成input file
+# 预计：50%吞吐丢包率为10%-30%（变量：zipf参数、流个数、平均每流包个数）
+
 with open("input.txt", "w") as f:
     f.write(str(int(5e3)) + '\n')
     f.write(str(int(1e4)) + '\n')
