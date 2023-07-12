7ae3e167ced39db55ba967496df2d882814b1ee8
jijinfanhua <1446353452@qq.com>
2023-07-12 19:51:39 +0800

    update generate

--- non_blocking_emulator/run_rapid.py
+++ non_blocking_emulator/run_rapid.py
@@ -6,4 +6,4 @@ for packet_num in range(10000, 100001, 10000):
         for zipfa in [round(i*0.01, 2) for i in range(101, 121)]:
             trace_name = f"syn_{packet_num}_{flow_num}_{cycle_num}_{zipfa}.txt"
             os.system(f"RAPID-SIM.exe 1 {trace_name}")
-            os.system(f"python get_average_queue_length.py {trace_name}")
+            # os.system(f"python get_average_queue_length.py {trace_name}")
