c134224efe6a8421e7b1d7fd60ef68611438aa0f
jijinfanhua <1446353452@qq.com>
2023-07-12 11:05:11 +0800

    update non_blocking

--- non_blocking_emulator/get_average_queue_length.py
+++ non_blocking_emulator/get_average_queue_length.py
@@ -1,10 +1,15 @@
-
+import os
+import sys
 
 # 首先获得数据包所在的时钟，然后读每个时钟的队列长度
 
 pkt_clk = []
 
-with open("output3.txt", 'r') as f:
+# 该文件需要参数化
+
+trace_name = sys.argv[1] #"output3.txt"
+
+with open("./trace_syn/" + trace_name, 'r') as f:
     while True:
         line = f.readline()
         if not line:
@@ -15,7 +20,7 @@ with open("output3.txt", 'r') as f:
 
 queue_clk_dict = {}
 
-with open("db_size_i_27_1q.txt", 'r') as g:
+with open("./trace_syn/" + "db_size_i_27_1q.txt", 'r') as g:
     while True:
         line = g.readline()
         if not line:
@@ -32,7 +37,7 @@ print("packet average queue length: {}".format(queue_sum / len(pkt_clk)))
 
 # 2. 求任意时刻队列长度，需要读取end_clock
 last_clk = 0
-with open("end_clock.txt", 'r') as h:
+with open("./trace_syn/" + "end_clock.txt", 'r') as h:
     last_clk = int(h.readline().strip())
 
 # 使用列表推导式选择所有小于 last_clk 的键对应的值
@@ -41,4 +46,10 @@ values = [v for k, v in queue_clk_dict.items() if k < last_clk]
 mean_value = sum(values) / len(values) if values else None
 
 print("average queue length: {}".format(mean_value))
+
+filename_without_extension, extension = os.path.splitext(trace_name)
+
+with open("./trace_syn/" + filename_without_extension + '_queue_info.txt', 'w') as t:
+    t.write("packet average queue length: {}\n".format(queue_sum / len(pkt_clk)))
+    t.write("average queue length: {}\n".format(mean_value))
 
